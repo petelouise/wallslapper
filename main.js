37f0afc495d65bdb2003a7dbaa34a6c5b4ac689b
petelouise <programmetress@petelouise.beauty>
2024-06-07 12:08:20 -0500

    Added main.js file.

---
+++ main.js
@@ -0,0 +1,27 @@
+import { setWallpaper } from "wallpaper"
+
+// await getWallpaper()
+
+import createCanvas from "canvas"
+import "fs"
+
+// Create a solid color image
+const width = 256
+const height = 256
+const canvas = createCanvas(width, height)
+const ctx = canvas.getContext("2d")
+ctx.fillStyle = "#00ff00"
+ctx.fillRect(0, 0, width, height)
+
+// Save the image
+const image_path = __dirname + "/solid_color.png"
+const out = fs.createWriteStream(image_path)
+const stream = canvas.createPNGStream()
+stream.pipe(out)
+
+// Set wallpaper
+out.on("finish", async () => {
+	await setWallpaper(image_path)
+})
+
+console.log("Done!")
