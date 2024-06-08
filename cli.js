133b42af2505fb49c9775c978604b8db7b782c58
petelouise <programmetress@petelouise.beauty>
2024-06-07 22:40:38 -0500

    Refactored CLI argument parsing to resolve options.

--- cli.js
+++ cli.js
@@ -8,57 +8,17 @@ import {
 	transitionToColor,
 } from "./wallpaperUtils.js"
 
-export async function parseCLIArgs() {
-	const argv = yargs(hideBin(process.argv))
-		.option("color", {
-			alias: "c",
-			type: "string",
-			description: "Specify a color to transition to immediately",
-		})
-		.option("schedule", {
-			alias: "s",
-			type: "boolean",
-			description: "Use a predefined schedule to transition to colors",
-		})
-		.option("duration", {
-			alias: "d",
-			description: "The duration of the transition in milliseconds",
-			type: "number",
-		})
-		.option("genconfig", {
-			alias: "g",
-			type: "boolean",
-			description: "Generate an example config file in the home directory",
-		})
-		.help()
-		.alias("help", "h").argv
-
+export async function resolveOptions(argv) {
 	const config = await readConfig()
 
-	if (argv.genconfig) {
-		await genconfig()
-	} else if (argv.color) {
-		const startColor = await readCurrentColor()
-		const endColor = argv.color
-		const duration = argv.duration !== undefined ? argv.duration : config.defaultTransitionTime
-
-		await transitionToColor(startColor, endColor, duration)
-	} else if (argv.schedule || argv.scheduleFile) {
-		const colorSchedule = argv.scheduleFile
-			? await readColorSchedule(argv.scheduleFile)
-			: config.defaultSchedule
-
-		if (colorSchedule) {
-			await transitionBasedOnTime(colorSchedule, config.defaultTransitionTime)
-		} else {
-			console.error(
-				"No schedule file provided and no default schedule found in config."
-			)
-		}
-	} else {
-		const startColor = getRandomColor()
-		const endColor = getRandomColor()
-		await transitionToColor(startColor, endColor, config.defaultTransitionTime)
-		console.log("Random transition complete!")
+	const options = {
+		genconfig: argv.genconfig || false,
+		color: argv.color || null,
+		schedule: argv.schedule || false,
+		scheduleFile: argv.scheduleFile || null,
+		duration: argv.duration !== undefined ? argv.duration : config.defaultTransitionTime,
+		defaultSchedule: config.defaultSchedule,
 	}
+
+	return options
 }
